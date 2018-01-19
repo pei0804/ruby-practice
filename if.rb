c1ad64827ef916bdee9321513a83c81351680a20
pei0804 <peeeei0804@gmail.com>
2018-01-19 17:08:58 +0900

    if

---
+++ if.rb
@@ -0,0 +1,19 @@
+=begin
+if 条件式1 then
+  # do something 1
+elsif 条件式2 then
+  # do something 2
+else
+  # do something 3
+end
+=end
+
+ans = 10
+
+if ans == 10
+  puts "#{ans}だったよ ans == 10"
+elsif ans > 10
+  puts "#{ans}だったよ ans > 10"
+else
+  puts "#{ans}だったよ"
+end
